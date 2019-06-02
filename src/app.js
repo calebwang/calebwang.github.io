de65e40f74549bbcbfe1ed3fc47426fb2caa63bc
Caleb Wang <caleb@caleb.wang>
2019-06-01 17:09:10 -0700

    set header padding via JS

--- src/app.js
+++ src/app.js
@@ -1,56 +1,99 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 import linkedin from './linkedin.png';
 import facebook from "./facebook.png";
 import instagram from "./instagram.png";
 import twitter from "./twitter.png";
 import github from "./github.png";
-import './app.css';
+import "./app.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.contentRef = React.createRef();
+    this.state = {
+      contentOffset: 0
+    };
+  }
+
+  resize() {
+    this.setState({
+      contentOffset: this.contentRef.current.contentOffset()
+    });
+  }
+
+  componentDidMount() {
+    this.resize();
+  }
+
   render() {
     return <div className="app-root">
-      <Header/>
-      <Content/>
+      <Header offset={this.state.contentOffset} />
+      <Content ref={this.contentRef} />
     </div>;
   }
 }
 
 class Header extends React.Component {
   render() {
+    const style = {
+      "padding-left": this.props.offset
+    };
     return <div className="header-container">
-      <div className="header">caleb wang</div>
+      <div className="header" style={style}>caleb wang</div>
     </div>;
   }
 }
 
 class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.referencesRef = React.createRef();
+  }
+
+  contentOffset() {
+    return this.referencesRef.current.contentOffset();
+  }
+
   render() {
-    return <div className="content-container">
-      <div className="content"><References/></div>
-    </div>
+    return <div className="content">
+      <ImageLinks ref={this.referencesRef}/>
+    </div>;
   }
 }
 
-class References extends React.Component {
+class ImageLinks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.firstImageLinkRef = React.createRef();
+  }
+
+  contentOffset() {
+    const myNode = ReactDOM.findDOMNode(this);     
+    const firstImageLink = this.firstImageLinkRef.current;
+    return firstImageLink.offsetLeft() - myNode.offsetLeft;
+  }
+
   render() {
     const items = [
-      <Reference
+      <ImageLink
         img={facebook}
         name="Facebook"
+        ref={this.firstImageLinkRef}
         url="https://www.facebook.com/calebwang"/>,
-      <Reference
+      <ImageLink
         img={linkedin}
         url="https://www.linkedin.com/in/calebdwang"
         name="LinkedIn"/>,
-      <Reference
+      <ImageLink
         img={instagram}
         url="https://www.instagram.com/calebdwang"
         name="Instagram"/>,
-      <Reference
+      <ImageLink
         img={twitter}
         url="https://www.twitter.com/calebdwang"
         name="Twitter"/>,
-      <Reference
+      <ImageLink
         img={github}
         url="https://www.github.com/calebwang"
         name="GitHub"/>
@@ -60,10 +103,19 @@ class References extends React.Component {
   }
 }
 
-class Reference extends React.Component {
+class ImageLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.domRef = React.createRef();
+  }
+
+  offsetLeft() {
+    return this.domRef.current.offsetLeft;
+  }
+  
   render() {
     return <div className="reference-container">
-      <a className="reference" href={this.props.url}>
+      <a className="reference" href={this.props.url} ref={this.domRef}>
         <img src={this.props.img} className="reference-img" alt={this.props.name}/>
       </a>
     </div>;
